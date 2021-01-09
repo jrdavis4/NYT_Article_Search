b779e10327f7e715641a80462e273660ad2087a3
Justin Davis <jrdavis4@gmail.com>
2021-01-09 13:32:02 -0500

    added functions and event listener. Still not tested

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,57 @@
- //assign variables
- var startYear = "";
- var endYear = "";
- var apiKey = "AcmPBVj10iUfqxs5czSPGoBnb96xiKmm";
- var articleDiv = $("#artivle-section");
-
- //inputs
- var Query = $("#searchTerm1").val();
-
- var count = parseInt($("#records1").val());
-
- var inputStart = $("#startYear1").val().trim();
- if (inputStart) {
-   startYear = "&begin_date=" + inputStart.toString() + "0101";
- }
+var articleDiv = $("#artivle-section");
  
- var inputEnd = $("#endYear1").val().trim();
- if (inputEnd) {
-   endYear = "&end_date=" + inputEnd.toString() + "1231";
- }
+function search () {
+  //assign variables
+  var startYear = "";
+  var endYear = "";
+  var apiKey = "AcmPBVj10iUfqxs5czSPGoBnb96xiKmm";
 
- var urlQuery = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + Query + startYear + endYear + "&api-key=" + apiKey;
+  //inputs
+  var Query = $("#searchTerm1").val();
 
- $.ajax({
-   url: urlQuery,
-   method: "GET"
- }).then(function(response) {
+  var count = parseInt($("#records1").val());
 
-   for (var i = 0; i < count; i++) {
-     var title = response.response.docs[i].headline.main;
-     var author = response.response.docs[i].byline.original;
-     var newDiv = $("<div>");
-     var newTitle = $("<h3>").html("<span id='article-number'>" + i + "</span>" + title);
-     var newAuthor = $("<h6>").text(author);
-     newDiv.append(newTitle, newAuthor);
+  var inputStart = $("#startYear1").val().trim();
+  if (inputStart) {
+    startYear = "&begin_date=" + inputStart.toString() + "0101";
+  }
 
-     articleDiv.append(newDiv);
-   }
+  var inputEnd = $("#endYear1").val().trim();
+  if (inputEnd) {
+    endYear = "&end_date=" + inputEnd.toString() + "1231";
+  }
 
- })
+  var urlQuery = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + Query + startYear + endYear + "&api-key=" + apiKey;
+
+  $.ajax({
+    url: urlQuery,
+    method: "GET"
+  }).then(function(response) {
+
+    for (var i = 0; i < count; i++) {
+      var title = response.response.docs[i].headline.main;
+      var author = response.response.docs[i].byline.original;
+      var newDiv = $("<div>");
+      var newTitle = $("<h3>").html("<span id='article-number'>" + i + "</span>" + title);
+      var newAuthor = $("<h6>").text(author);
+      newDiv.append(newTitle, newAuthor);
+
+      articleDiv.append(newDiv);
+    }
+
+  })
+}
+
+function clear() {
+  //code to clear results
+}
+ 
+ 
+
+$(".btn").click(function(){
+  if ($(this).attr("type") === "search") {
+    search();
+  } else if ($(this).attr("type") === "clear") {
+    clear();
+  }
+})
